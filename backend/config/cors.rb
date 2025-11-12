c42bf59e0b89ff54e290ddbff9c6a2024277296e
Viet <viet.do@saleshood.com>
2025-11-12 13:54:07 +0700

    feat: add CORS support with rack-cors gem and configuration

---
+++ backend/config/cors.rb
@@ -0,0 +1,9 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'localhost:5173', '127.0.0.1:5173'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
