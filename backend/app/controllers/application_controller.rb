b9f0feadf48a50f61f21168a09b7e85c03dabdc0
Viet <viet.do@saleshood.com>
2025-11-14 09:49:03 +0700

    Add Devise for user authentication
    - Updated Gemfile to include Devise gem.
    - Added Devise configuration initializer.
    - Created User model with Devise modules for authentication.
    - Implemented user registration, login, password recovery, and email confirmation views.
    - Added routes for Devise user authentication.
    - Created migration to add Devise fields to users table.
    - Updated schema to reflect changes in the users table.
    - Added error messages and shared links for Devise views.

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   # allow_browser versions: :modern unless Rails.env.test?
   skip_before_action :verify_authenticity_token
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
