8a37b2d3dc61164fdf3f180be931705ce3fe2ccf
Viet Do <vietdqhcmute@gmail.com>
2025-11-17 10:33:40 +0700

    Update CORS to support all Vercel domains with regex pattern

--- backend/config/cors.rb
+++ backend/config/cors.rb
@@ -1,10 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:5173', '127.0.0.1:5173', 'https://access-interview-proj.vercel.app'
+    origins %r{https://.*\.vercel\.app}, 'localhost:5173', '127.0.0.1:5173', /http:\/\/localhost(:\d+)?/
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      credentials: true,
+      expose: ['Authorization']
   end
 end
