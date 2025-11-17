9528ca2e94cf77ee2ed0efae9e5187534218799d
Viet Do <vietdqhcmute@gmail.com>
2025-11-17 09:43:39 +0700

    Fix Redis SSL connection for Heroku

--- backend/config/initializers/sidekiq.rb
+++ backend/config/initializers/sidekiq.rb
@@ -1,7 +1,17 @@
 require 'sidekiq/cron/web'
 
+# Configure Redis connection for Heroku
+redis_config = {
+  url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0')
+}
+
+# Add SSL configuration for Heroku Redis
+if ENV['REDIS_URL']&.start_with?('rediss://')
+  redis_config[:ssl_params] = { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+end
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0') }
+  config.redis = redis_config
 
   # Enable sidekiq-cron poller
   config.on(:startup) do
@@ -13,5 +23,5 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0') }
+  config.redis = redis_config
 end
