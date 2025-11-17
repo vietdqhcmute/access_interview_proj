59014dc71448ff6ba07a31c16370a34645da7929
Viet Do <vietdqhcmute@gmail.com>
2025-11-17 10:17:49 +0700

    Add Vercel domain to CORS configuration

--- backend/config/cors.rb
+++ backend/config/cors.rb
@@ -1,9 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:5173', '127.0.0.1:5173'
+    origins 'localhost:5173', '127.0.0.1:5173', 'https://access-interview-proj.vercel.app'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end
