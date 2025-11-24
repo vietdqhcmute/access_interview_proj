7316810f6acad855d8bcf4612436120d0c5914bb
Viet Do <vietdqhcmute@gmail.com>
2025-11-24 14:57:11 +0700

    chore: set up testing environment with Vitest and Jest-DOM
    - Added setupTests.ts to configure Jest-DOM and mock window.matchMedia for Ant Design components.
    - Updated tsconfig.app.json to include Vitest globals for type definitions.
    - Configured Vite to use the new setup file for testing, enabling jsdom environment and global variables.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    setupFiles: './src/setupTests.ts',
+    environment: 'jsdom',
+    globals: true,
+    coverage: {},
+  },
   server: {
     host: true,
     port: 5173,
